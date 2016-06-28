989df0853e2c71492734fdc8378dd93904cb5c85
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2016-06-27 22:46:41 -0400

    added run sweep bash file

---
+++ run_sweep.sh
@@ -0,0 +1,4 @@
+python python/gps/gps_main.py mjc_badmm_example
+python python/gps/gps_main.py mjc_badmm_example1
+python python/gps/gps_main.py mjc_badmm_example2
+python python/gps/gps_main.py mjc_badmm_example3
